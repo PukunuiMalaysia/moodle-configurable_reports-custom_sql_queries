04a29bab8323752d9105689e35ce39017dc2915e
Bob <bob@Bobs-Virtual-Machine.local>
2026-03-24 16:31:16 +0800

    Add course role assignments report with last access

---
+++ user/course-role-assignments-with-last-access.sql
@@ -0,0 +1,21 @@
+SELECT 
+ u.firstname AS "First Name",
+ u.lastname AS "Last Name",
+ u.email AS "Email",
+ r.shortname AS "Role",
+ c.fullname AS "Course",
+ FROM_UNIXTIME(ul.timeaccess) AS "Last Access"
+FROM
+ prefix_role_assignments ra
+JOIN
+ prefix_user u ON u.id = ra.userid
+JOIN
+ prefix_role r ON r.id = ra.roleid
+JOIN
+ prefix_context ctx ON ctx.id = ra.contextid
+JOIN
+ prefix_course c ON c.id = ctx.instanceid
+JOIN
+ prefix_user_lastaccess ul ON ul.userid = ra.userid AND ul.courseid = ctx.instanceid
+WHERE
+ ctx.contextlevel = 50
