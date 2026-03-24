0cd1a18196819d2e4824ed84183abaff73c2bf20
Bob <bob@Bobs-Virtual-Machine.local>
2026-03-24 16:19:51 +0800

    Add report for active courses with category and start date

---
+++ course/active-courses-with-category-and-start-date.sql
@@ -0,0 +1,14 @@
+SELECT 
+ c.id AS 'Course ID',
+ c.fullname AS 'Course Name',
+ c.shortname AS 'Course Short Name',
+ cc.name AS 'Category',
+ FROM_UNIXTIME(c.startdate, '%Y-%m-%d') AS 'Start Date'
+FROM 
+ prefix_course c
+JOIN 
+ prefix_course_categories cc ON c.category = cc.id
+WHERE 
+ c.visible = 1
+ORDER BY 
+ c.fullname ASC
