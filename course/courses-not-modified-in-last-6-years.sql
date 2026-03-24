ccab8cb099158280d6164d5c560a08a3cb40a71e
Bob <bob@Bobs-Virtual-Machine.local>
2026-03-24 17:08:34 +0800

    Add report for courses not modified in the last 6 years

---
+++ course/courses-not-modified-in-last-6-years.sql
@@ -0,0 +1,5 @@
+SELECT c.id, c.fullname, c.shortname, FROM_UNIXTIME(c.timemodified) AS last_modified, 
+CONCAT('<a href="%%WWWROOT%%/course/view.php?id=', c.id, '" target="_blank">', c.fullname, '</a>') AS course_link
+FROM prefix_course AS c
+WHERE c.timemodified < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 6 YEAR))
+ORDER BY c.timemodified
